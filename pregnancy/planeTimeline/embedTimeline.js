8c8d1eaafae5b6378cfd9e30cc7665dc8ae68ff9
Ed Cole <EdwardWCole@gmail.com>
2015-10-31 15:48:33 -0400

    Widget

--- pregnancy/planeTimeline/embedTimeline.js
+++ pregnancy/planeTimeline/embedTimeline.js
@@ -2,9 +2,9 @@
 var embedTimeline = function (divId, dueDate, appHome) {
     var root = appHome?appHome:'';
     var div = document.getElementById(divId);
-    var iframe = document.createElement('iframe')
-    iframe.width = "620px";
-    iframe.height = "203px";
+    var iframe = document.createElement('iframe');
+    iframe.width = "907px";
+    iframe.height = "208px";
     iframe.style = "border: none;";
     div.appendChild(iframe);
     iframe.src=appHome + "innerPregnancyTimeline.html#" + dueDate;

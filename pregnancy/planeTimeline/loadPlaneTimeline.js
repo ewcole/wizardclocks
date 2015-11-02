bd901095ade592c0f5bd9ca357330c99dad46b39
Ed Cole <edcole@mail.sunyjcc.edu>
2015-11-02 08:50:13 -0500

    Develop widget for pregnancy timeline (interrupted by closure at work).

--- pregnancy/planeTimeline/loadPlaneTimeline.js
+++ pregnancy/planeTimeline/loadPlaneTimeline.js
@@ -1,10 +1,13 @@
-var f = function (div, dueDate) {
+var pregnancyTimelineLoader = function (div, dueDate, local) {
+    var root = "http://ewcole.github.io/wizardclocks/pregnancy/planeTimeline/";
+    root = local?"":root;
+root = "";
     var clockDiv = document.getElementById(div);
     var iFrame = document.createElement('iframe');
     clockDiv.appendChild(iFrame);
     iFrame.width  = "620px";
     iFrame.height = "203px";
     iFrame.style.border = "none";
-    iFrame.src = "pregnancyTimeline.html#2016-06-03";
+    iFrame.src = root + "embeddedPregnancyTimeline.html";// + dueDate;
     return;
-}();
+};
